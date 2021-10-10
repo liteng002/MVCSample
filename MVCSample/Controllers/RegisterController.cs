e0a7f7419cfd1f95ae0b64a13554b9a793f9ff31
965330241@qq.com <965330241@qq.com>
2021-10-10 19:26:00 +0800

    Add project files.

---
+++ MVCSample/Controllers/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using MVCSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSample.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            RegisterModel model = new RegisterModel
+            {
+                Name = "飞哥",
+                Password = "1234"
+            };
+            return View(model);
+        }
+    }
+}
